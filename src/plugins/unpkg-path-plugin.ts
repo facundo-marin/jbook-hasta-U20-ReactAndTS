de8871e394537c4e4b9556f2f2fe19e9e8a88609
Facundo Marin <facundo.marin@moxit.net>
2022-08-09 11:28:06 -0300

    8.10. Bonus! Package Versioning

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -34,7 +34,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('react');
+              const message = require('react@16.0.0');
               console.log(message);
             `,
           };
