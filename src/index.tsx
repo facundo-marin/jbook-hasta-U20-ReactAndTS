b8374426001994fad06b33c4d110a0fcb4d4306c
Facundo Marin <facundo.marin@moxit.net>
2022-08-10 14:53:30 -0300

    11.9.  Handling editor change Events

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
 import { fetchPlugin } from './plugins/fetch-plugin';
@@ -87,11 +87,4 @@ const App = () => {
   );
 };
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
