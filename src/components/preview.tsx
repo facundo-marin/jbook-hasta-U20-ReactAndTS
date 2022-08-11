f0cbfbaba773fe9e25bc40d70a307600d2c7447b
Facundo Marin <facundo.marin@moxit.net>
2022-08-11 12:45:04 -0300

    13.7. Fixing the resize issue

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from 'react';
+import './preview.css';
 
 interface PreviewProps {
   code: string;
@@ -33,13 +34,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-      style={{ backgroundColor: 'white' }}
-      ref={iframe}
-      sandbox="allow-scripts"
-      title="preview"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        ref={iframe}
+        sandbox="allow-scripts"
+        title="preview"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
