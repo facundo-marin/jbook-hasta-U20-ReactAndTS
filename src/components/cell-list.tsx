39bad3a8314dd09c111d4b091fdbaf9be483863a
Facundo Marin <facundo.marin@moxit.net>
2022-08-16 10:11:32 -0300

    18.3. Selecting an Ordered List

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,9 @@
+import { useTypedSelector } from '../hooks/use-typed-selector';
+
 const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
   return <div>Cell List</div>;
 };
 
