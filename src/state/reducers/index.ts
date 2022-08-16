19fcfc5f19768e7009813f271443d3a2676260af
Facundo Marin <facundo.marin@moxit.net>
2022-08-16 18:31:16 -0300

    19.5. Finishing up de reducer

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import celssReducer from './cellsReducer';
+import bundlesReducer from './bundlesReducer';
 
 const reducers = combineReducers({
   cells: celssReducer,
+  bundles: bundlesReducer,
 });
 
 export default reducers;
