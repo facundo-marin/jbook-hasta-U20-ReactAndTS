fa97fbc576c2f6ed5d11caf8aab309435450fbec
Facundo Marin <facundo.marin@moxit.net>
2022-08-16 12:15:30 -0300

    18.11. Applyng Custom CSS

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,3 +1,4 @@
+import './cell-list-item.css';
 import { Cell } from '../state';
 import TextEditor from './text-editor';
 import CodeCell from './code-cell';
@@ -15,9 +16,9 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
     child = <TextEditor cell={cell} />;
   }
   return (
-    <div>
-      <ActionBar id={cell.id} />
+    <div className="cell-list-item">
       {child}
+      <ActionBar id={cell.id} />
     </div>
   );
 };
