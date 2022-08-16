9e1cb4da25bd3ee6ae67ce5d8e0d561cea2b5800
Facundo Marin <facundo.marin@moxit.net>
2022-08-16 16:43:15 -0300

    18.24. Refactoring the React Side

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,18 +2,18 @@ import './add-cell.css';
 import { useActions } from './../hooks/use-actions';
 
 interface AddCellProps {
-  nextCellId: string | null;
+  previousCellId: string | null;
   forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ forceVisible, nextCellId }) => {
-  const { insertCellBefore } = useActions();
+const AddCell: React.FC<AddCellProps> = ({ forceVisible, previousCellId }) => {
+  const { insertCellAfter } = useActions();
   return (
     <div className={`add-cell ${forceVisible && 'force-visible'}`}>
       <div className="add-buttons">
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, 'code')}
+          onClick={() => insertCellAfter(previousCellId, 'code')}
         >
           <span className="icon is-small">
             <i className="fa fa-plus"></i>
@@ -23,7 +23,7 @@ const AddCell: React.FC<AddCellProps> = ({ forceVisible, nextCellId }) => {
 
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, 'text')}
+          onClick={() => insertCellAfter(previousCellId, 'text')}
         >
           <span className="icon is-small">
             <i className="fa fa-plus"></i>
