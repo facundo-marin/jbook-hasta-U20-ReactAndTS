b70cfc0984032ba417be1b384936228988f01463
Facundo Marin <facundo.marin@moxit.net>
2022-08-16 10:29:52 -0300

    18.5. Cells Type rendering

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,11 +1,19 @@
 import { Cell } from '../state';
+import TextEditor from './text-editor';
+import CodeCell from './code-cell';
 
 interface CellListItemProps {
   cell: Cell;
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
-  return <div>{cell.id}</div>;
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+  return <div>{child}</div>;
 };
 
 export default CellListItem;
