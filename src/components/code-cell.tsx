4376e28c3ff550ab06407c300249a9e78278e9f8
Facundo Marin <facundo.marin@moxit.net>
2022-08-17 11:51:16 -0300

    19.13. Styling the progress bar

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import React, { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -46,7 +47,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
           />
         </Resizable>
         {!bundle || bundle.loading ? (
-          <div>Loading...</div>
+          <div className="progress-cover">
+            <progress className="progress is-small is-primary" max="100">
+              Loading
+            </progress>
+          </div>
         ) : (
           bundle && <Preview code={bundle.code} error={bundle.error} />
         )}
